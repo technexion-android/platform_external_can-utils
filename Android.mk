35d058ac48c4e8623ee9acf6a23ae280f731ae1c
Wig Cheng <wig.cheng@technexion.com>
2019-07-10 14:28:11 +0800

    can-utils: fix c99 compatible issue

--- Android.mk
+++ Android.mk
@@ -1,6 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 
-PRIVATE_LOCAL_CFLAGS := -O2 -g -W -Wall		\
+PRIVATE_LOCAL_CFLAGS := -O2 -g -W -Wall -Werror		\
 			-DSO_RXQ_OVFL=40	\
 			-DPF_CAN=29		\
 			-DAF_CAN=PF_CAN
